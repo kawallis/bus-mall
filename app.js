2146071f20a59385cc4d8294025af31cc0929cb0
Chris_Wallace <kawallis@icloud.com>
2017-02-21 17:18:39 -0800

    added a chart to display top five most clicked

--- app.js
+++ app.js
@@ -5,41 +5,41 @@ var image_two = document.getElementById('image_two');
 var image_three = document.getElementById('image_three');
 var blank_arr = [];
 var global_clicked = 0;
-var global_timesShown = 0;
 
-function Picture (name, filepath, id_num) {
+
+function Picture (name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  this.id_num = id_num;
   this.timesClicked = 0;
   this.timesShown = 0;
 }
 
-var item_one = new Picture ('r2d2_bag','img/bag.jpg', 0);
-var item_two = new Picture ('banana', 'img/banana.jpg', 1);
-var item_three = new Picture ('bathroom', 'img/bathroom.jpg', 2);
-var item_four = new Picture ('boots', 'img/boots.jpg', 3);
-var item_five = new Picture ('breakfest', 'img/breakfast.jpg', 4);
-var item_six = new Picture ('bubblegum', 'img/bubblegum.jpg', 5);
-var item_seven = new Picture ('chair', 'img/chair.jpg', 6);
-var item_eight = new Picture ('cthulhu', 'img/cthulhu.jpg', 7);
-var item_nine = new Picture ('dog-duck', 'img/dog-duck.jpg', 8);
-var item_ten = new Picture ('dragon', 'img/dragon.jpg', 9);
-var item_eleven = new Picture ('pen', 'img/pen.jpg', 10);
-var item_twelve = new Picture ('pet-sweep', 'img/pet-sweep.jpg', 11);
-var item_thirteen = new Picture ('scissors', 'img/scissors.jpg', 12);
-var item_fourteen = new Picture ('shark', 'img/shark.jpg', 13);
-var item_fifteen = new Picture ('sweep', 'img/sweep.png', 14);
-var item_sixteen = new Picture ('tauntaun.jpg', 'img/tauntaun.jpg', 15);
-var item_seventeen = new Picture ('unicorn', 'img/unicorn.jpg', 16);
-var item_eighteen = new Picture ('usb', 'img/usb.gif', 17);
-var item_nineteen = new Picture ('water-can', 'img/water-can.jpg', 18);
-var item_twenty = new Picture ('wine-glass', 'img/wine-glass.jpg', 19);
+var item_one = new Picture ('r2d2_bag','img/bag.jpg');
+var item_two = new Picture ('banana', 'img/banana.jpg');
+var item_three = new Picture ('bathroom', 'img/bathroom.jpg');
+var item_four = new Picture ('boots', 'img/boots.jpg');
+var item_five = new Picture ('breakfest', 'img/breakfast.jpg');
+var item_six = new Picture ('bubblegum', 'img/bubblegum.jpg');
+var item_seven = new Picture ('chair', 'img/chair.jpg');
+var item_eight = new Picture ('cthulhu', 'img/cthulhu.jpg');
+var item_nine = new Picture ('dog-duck', 'img/dog-duck.jpg');
+var item_ten = new Picture ('dragon', 'img/dragon.jpg');
+var item_eleven = new Picture ('pen', 'img/pen.jpg');
+var item_twelve = new Picture ('pet-sweep', 'img/pet-sweep.jpg');
+var item_thirteen = new Picture ('scissors', 'img/scissors.jpg');
+var item_fourteen = new Picture ('shark', 'img/shark.jpg');
+var item_fifteen = new Picture ('sweep', 'img/sweep.png');
+var item_sixteen = new Picture ('tauntaun', 'img/tauntaun.jpg');
+var item_seventeen = new Picture ('unicorn', 'img/unicorn.jpg');
+var item_eighteen = new Picture ('usb', 'img/usb.jpg');
+var item_nineteen = new Picture ('water-can', 'img/water-can.jpg');
+var item_twenty = new Picture ('wine-glass', 'img/wine-glass.jpg');
 
 var object_arr = [item_one, item_two, item_three, item_four, item_five,
   item_six, item_seven, item_eight, item_nine, item_ten, item_eleven,
   item_twelve, item_thirteen, item_fourteen, item_fifteen, item_sixteen,
   item_seventeen, item_eighteen, item_nineteen, item_twenty];
+
 
 var picture_arr = [image_one, image_two, image_three];
 
@@ -69,10 +69,11 @@ function render () {
   }
   image_three.src = random_three;
   blank_arr.push(random_three);
+
+  //adds up the number of times an image is shown
   for (var i = 0; i < object_arr.length; i++) {
     if (object_arr[i].filepath === random_one || object_arr[i].filepath === random_two || object_arr[i].filepath === random_three) {
       object_arr[i].timesShown += 1;
-      global_timesShown ++;
     }
   }
 }
@@ -99,11 +100,64 @@ function picturehandler (event) {
     }
     global_clicked++;
   } else {
-    alert('your done');
+    var bye_message = document.getElementById('heading');
+    bye_message.textContent = 'Thanks For Taking The Survey';
+    var container = document.getElementById('container-two');
+    container.textContent = '';
+    container.style.backgroundColor = 'white';
+    var chart = document.createElement('canvas');
+    chart.setAttribute('id', 'myChart');
+    container.appendChild(chart);
+
+    renderChart();
   }
 }
 
+// Building the chart
+function renderChart() {
+  var sorted_arr = [];
+  for (var i = 0; i < object_arr.length; i++) {
+    sorted_arr.push([object_arr[i].name, object_arr[i].timesClicked]);
+    console.log(object_arr[i].name);
+    sorted_arr.sort(function(a, b) {
+      return b[1] - a[1];
+    });
+  };
 
+  sorted_arr = sorted_arr.slice(0,8);
+  //makes array for chart labels
+  var item_name_arr = sorted_arr.map(function(item) {
+    return item[0] ;
+  });
 
+  // makes array for chart labels height of bar
+  var data_arr = sorted_arr.map(function(item) {
+    return item[1] ;
+  });
 
-//.includes('specialword');
+// makes a array of color codes
+  var color_arr = ['#FC4A1A', '#F7B733', '#4ABDAC', '#FC4A1A', '#F7B733', '#4ABDAC',
+    '#FC4A1A', '#F7B733'];
+
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: item_name_arr,
+      datasets: [{
+        label: 'Number of Clicks',
+        data: data_arr,
+        backgroundColor: color_arr
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+};
